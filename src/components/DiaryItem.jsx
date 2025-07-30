cfde25b8803c56a6829d6e62963d8b78cde2312c
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-07-30 22:30:04 +0900

    DiaryItem style 적용

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -4,14 +4,16 @@ import { DiaryStateContext } from "../App";
 import Button from "./Button";
 import { getEmotionImage } from "../util/get-emotion-image";
 
-const DiaryItem = ({ id, createdData, emotionId, content }) => {
+const DiaryItem = () => {
   const diaryItems = useContext(DiaryStateContext);
   console.log(diaryItems);
 
+  const emotionId = 2;
+
   return (
     <div className="DiaryItem">
-      <div className="img_section">
-        <img src={getEmotionImage(1)} />
+      <div className={`img_section img_section_${emotionId}`}>
+        <img src={getEmotionImage(emotionId)} />
       </div>
       <div className="info_section">
         <div className="created_date">{new Date().toLocaleDateString()}</div>
