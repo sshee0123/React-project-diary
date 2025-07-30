ebb130905d88239b246b0bf23411cc73682ee535
say <sshee0123@naver.com>
2025-07-30 10:40:00 +0900

    동적 경로로 페이지 라우팅

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,5 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <div>Diary</div>;
+  // useParams
+  // 현재 브라우저가 명시한 파라미터값 가져오는 역할
+  const params = useParams();
+
+  return <div>{params.id}번 일기입니다.</div>;
 };
 
 export default Diary;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+  // useSearchParams
+  // useState처럼 활용 가능
+  const [params, setParams] = useSearchParams();
+  console.log(params.get("value")); // hello 출력
   return <div>Home</div>;
 };
 
