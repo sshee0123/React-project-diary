b0f4964737857897ce4fc03e5c60a26173d0f3cb
say <sshee0123@naver.com>
2025-07-30 10:07:41 +0900

    라우팅 설정하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Notfound from "./pages/Notfound";
 
+// 1. "/" : 모든 일기를 조회하는 HOME 페이지
+// 2. "/new" :  새로운 일기를 작성하는 NEW 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+// 4. "*" :  whildcard(*) : 잘못된 페이지 조회시
+// Routes 컴포넌트 자식은 <Route>만 가능
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/new" element={<New></New>}></Route>
+      <Route path="/diary" element={<Diary></Diary>}></Route>
+      <Route path="*" element={<Notfound></Notfound>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+// BrowserRouter
+// -> 브라우저의 현재 주소를 저장하고 감지하는 역할
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
