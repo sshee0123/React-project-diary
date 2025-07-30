19713e9c5ce9b737009fe5555e7779a7ea906d2d
say <sshee0123@naver.com>
2025-07-30 11:07:07 +0900

    폰트, 이미지, 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 
+import { getEmotionImage } from "./util/get-emotion-image";
+
 // 1. "/" : 모든 일기를 조회하는 HOME 페이지
 // 2. "/new" :  새로운 일기를 작성하는 NEW 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
@@ -21,6 +23,13 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
