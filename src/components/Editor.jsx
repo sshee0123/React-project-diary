0ff2a65de1e9de3b8405df49e7f11841832b2247
say <sshee0123@naver.com>
2025-07-31 17:52:09 +0900

    New페이지 기능 구현 1 (날짜변경)

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,6 +2,8 @@ import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
 
+import { useState } from "react";
+
 // EmotionItem 리스트
 const emotionList = [
   {
@@ -26,19 +28,71 @@ const emotionList = [
   },
 ];
 
+// 문자열로 변환된 날짜 구하는 함수
+const getStringedDate = (targetDate) => {
+  // 날짜 -> YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
 const Editor = () => {
+  // 사용자가 입력한 오늘의 날짜, 감정, 일기를 객체로 useState 활용해 저장
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  // input 수정 시 이벤트 핸들러
+  const onChangeInput = (e) => {
+    // e.target.value는 객체라 날짜로 형변환 해줘야함.
+    let name = e.target.name; // 어떤 요소에 입력이 들어온건지
+    let value = e.target.value; // 입력한 값이 무엇인지
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
   const emotionId = 1;
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
           {emotionList.map((item) => (
             <EmotionItem
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                })
+              }
               key={item.emotionId}
               {...item}
               isSelected={item.emotionId === emotionId}
