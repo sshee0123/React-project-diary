76bee0b57ca556b460214830d848e76ee33411f0
say <sshee0123@naver.com>
2025-08-01 13:45:38 +0900

    New페이지 기능 구현 2 (입력값으로 새로운 일기 작성)

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,8 +1,8 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
-
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 // EmotionItem 리스트
 const emotionList = [
@@ -45,7 +45,9 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = () => {
+const Editor = ({ onSubmit }) => {
+  const nav = useNavigate();
+
   // 사용자가 입력한 오늘의 날짜, 감정, 일기를 객체로 useState 활용해 저장
   const [input, setInput] = useState({
     createdDate: new Date(),
@@ -53,7 +55,7 @@ const Editor = () => {
     content: "",
   });
 
-  // input 수정 시 이벤트 핸들러
+  // input 수정 이벤트 핸들러
   const onChangeInput = (e) => {
     // e.target.value는 객체라 날짜로 형변환 해줘야함.
     let name = e.target.name; // 어떤 요소에 입력이 들어온건지
@@ -68,7 +70,12 @@ const Editor = () => {
     });
   };
 
-  const emotionId = 1;
+  // 새 일기 작성 : 작성 완료 버튼
+  const onClickSubmitButton = () => {
+    // 부모 컴퍼넌트(New)에게 input값 넘겨주기
+    onSubmit(input);
+  };
+
   return (
     <div className="Editor">
       <section className="date_section">
@@ -95,18 +102,27 @@ const Editor = () => {
               }
               key={item.emotionId}
               {...item}
-              isSelected={item.emotionId === emotionId}
+              isSelected={item.emotionId === input.emotionId}
             />
           ))}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          onChange={onChangeInput}
+          name="content"
+          value={input.content}
+          placeholder="오늘은 어땠나요?"
+        />
       </section>
       <section className="button_section">
-        <Button text={"취소하기"} />
-        <Button text={"작성완료"} type={"POSITIVE"} />
+        <Button onClick={() => nav(-1)} text={"취소하기"} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={"작성완료"}
+          type={"POSITIVE"}
+        />
       </section>
     </div>
   );
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,19 +1,31 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
-
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
   // 페이지 뒤로 가기 nav(-1)
   const nav = useNavigate();
+
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  // Editor 컴포넌트에서 새로운 일기 작성완료 로 전송한 input값
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    // 작성 완료 버튼 누르고 Home 페이지 가기
+    // nav() replace : 뒤로 가기(다시 Edit페이지) 방지
+    nav("/", { replace: true });
+  };
+
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
         leftChild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
